1bd1fbaa017dff514e0f669b5154a76e1ef57e66
santiago <santiago.sosa@uptc.edu.co>
2021-03-11 10:00:59 -0500

    Mas pruebas

--- createNewInstance/createInstance.sh
+++ createNewInstance/createInstance.sh
@@ -7,7 +7,7 @@ cut -c 22-33 test.txt > ips.txt
 
 while read line; do
 	sed -i "/$line/d" ./ips.txt
-done < excluded.txt
+done < /home/serverone/Lab3Instancia/createNewInstance/excluded.txt
 
 echo "NO IP" > test.txt
 
@@ -20,9 +20,12 @@ while read line1; do
         then
        	        echo "Se conecto a una IP!!!"
                	echo "$line1" > test.txt
-		tranferir mi mi respaldo
-		restaurarlo con mongo
-		enviar la nueva ip al middleware
+		#tranferir mi mi respaldo
+		scp -rp /home/serverone/Lab3Instancia/restaurar/dump "serverone@$line1:"/home/serverone
+		#restaurarlo con mongo
+		sshpass -p 'a123' timeout 5s ssh "serverone@$line1" mongorestore
+		#reemplazar la nueva ip en el middleware
+		curl --data "[{ip:"$line1"}]" http://192.168.0.16:3000/changeServer
 		break
         fi
 done < ips.txt
--- monitor/verificar.sh
+++ monitor/verificar.sh
@@ -1,7 +1,7 @@
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
 
-curl -f 192.168.0.18:3000/ > prueba.txt
+curl -f 192.168.0.16:3000/ > prueba.txt
 
 var=$(cut -c 1 prueba.txt)
 
@@ -14,7 +14,12 @@ then
 	res="$res El servidor: 192.168.0.18 esta funcionando"
 else
 	res="$res El servidor 192.168.0.18 HA DEJADO DE FUNCIONAR"
+	#se crea la maquina virtual y doy tiempo para que la maquina inicie
+	sh /home/serverone/Lab3Instancia/createNewInstance/createvirtual.sh
+	sleep 30s
+	#busco la nuevo ip inicio el servicio, restura la bd y cambio la ip
 	sh /home/serverone/Lab3Instancia/createNewInstance/createInstance.sh
+	
 
 fi
 
